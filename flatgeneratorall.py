553ff345c87aced77f1c6180ea549ac6cfa94a20
Titulin Alexander <tito-1984@mail.ru>
2023-01-23 08:49:32 +0300

    initial commit

---
+++ flatgeneratorall.py
@@ -0,0 +1,29 @@
+import types
+from collections.abc import Iterable
+
+
+def flat_generator(list_of_list):
+    for i in list_of_list:
+        if isinstance(i, Iterable) and not isinstance(i, (str, bytes)):
+            yield from flat_generator(i)
+        else:
+            yield i
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
---
+++ main.py
@@ -0,0 +1,8 @@
+from flatiterator import test_1
+from flatgenerator import test_2
+from flatgeneratorall import test_4
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    test_4()
